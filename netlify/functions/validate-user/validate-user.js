daedacf1831e85777a47783a1fca1efc5569905f
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 22:59:57 +0200

    Different roles

--- netlify/functions/validate-user/validate-user.js
+++ netlify/functions/validate-user/validate-user.js
@@ -1,14 +1,23 @@
 exports.handler = async (event, context) => {
   const { user } = context.clientContext;
+  console.log('EVENT: ', event)
   console.log('CONTEXT: ', context)
-  console.log('META: ', user.user_metadata)
+  console.log('USER_META: ', user.user_metadata)
+  console.log('APP_META: ', user.app_metadata)
   if (user) {
     const userID = user.sub;
+    const { roles } = user.app_metadata
+    let userRole = "user"
+
+    if (roles.includes('admin')) {
+      userRole = 'admin'
+    }
+
     return {
       statusCode: 200,
       body: JSON.stringify({
         "X-Hasura-User-Id": userID,
-        "X-Hasura-Role": "user"
+        "X-Hasura-Role": userRole
       })
     };
   }
