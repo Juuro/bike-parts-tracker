97443c1308b454e93ca88b0a7d037955f3050a4b
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 21:34:26 +0200

    fix validate-user?

--- netlify/functions/validate-user/validate-user.js
+++ netlify/functions/validate-user/validate-user.js
@@ -1,5 +1,10 @@
-exports.handler = async (context) => {
-  const { user } = context.clientContext;
+exports.handler = async (event, context) => {
+  console.log('EVENT: ', event)
+  console.log('CONTEXT: ', context)
+  const {
+    identity,
+    user
+  } = context.clientContext;
   if (user) {
     const userID = user.sub;
     return {
