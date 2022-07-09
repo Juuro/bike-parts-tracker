a77a9a27df58e454886d5b8537a1d79a09a3dd4a
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 04:28:23 +0200

    user

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -4,62 +4,28 @@
 // more:
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
-// import fetch from "node-fetch";
 
-const handler = async function (event, context) {
-  const { identity, user } = context.clientContext;
-  
+const handler = async function (event) {
+  const data = JSON.parse(event.body)
+  const { user } = data
+
   console.log('USER: ', user)
 
-  // const { user } = JSON.parse(event.body);
-
-  // console.log('user: ', user)
-
-  // const responseBodyString = JSON.stringify({
-  //   query: `
-  //   mutation insertUser($id: String, $email:String, $name:String){
-  //     insert_bikepartstracker_user(objects: {id: $id, email: $email, name: $name}) {
-  //       affected_rows
-  //     }
-  //   }    
-  // `,
-  //   variables: {
-  //     id: user.id,
-  //     email: user.email,
-  //     name: user.user_metadata.full_name
-  //   }
-  // });
-
-  // console.log(responseBodyString);
-
-  // const result = await fetch(
-  //   "https://neutral-stud-43.hasura.app/v1/graphql",
-  //   {
-  //     method: "POST",
-  //     body: responseBodyString,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "x-hasura-admin-secret":
-  //         process.env.HASURA_SECRET
-  //     }
-  //   }
-  // );
-  // const { errors, data } = await result.json();
-
-  // if (errors) {
-  //   console.log(errors);
-  //   return {
-  //     statusCode: 500,
-  //     body: "Something is wrong"
-  //   };
-  // } else {
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify(responseBody)
-  //   };
-  // }
-
-  return { statusCode: 200 }
+  const responseBody = {
+    app_metadata: {
+      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
+      my_user_info: 'this is some user info',
+    },
+    user_metadata: {
+      // append current user metadata
+      ...user.user_metadata,
+      custom_data_from_function: 'hurray this is some extra metadata',
+    },
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  }
 }
 
-export default { handler }
+module.exports = { handler }
