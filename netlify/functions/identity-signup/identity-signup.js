4a635b862788d1059f4cf9c888737da25e56448e
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 04:22:04 +0200

    test

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -4,56 +4,62 @@
 // more:
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 const handler = async function (event, context) {
-  const { user } = JSON.parse(event.body);
+  const { identity, user } = context.clientContext;
+  
+  console.log('USER: ', user)
 
-  console.log('user: ', user)
+  // const { user } = JSON.parse(event.body);
 
-  const responseBodyString = JSON.stringify({
-    query: `
-    mutation insertUser($id: String, $email:String, $name:String){
-      insert_bikepartstracker_user(objects: {id: $id, email: $email, name: $name}) {
-        affected_rows
-      }
-    }    
-  `,
-    variables: {
-      id: user.id,
-      email: user.email,
-      name: user.user_metadata.full_name
-    }
-  });
+  // console.log('user: ', user)
 
-  console.log(responseBodyString);
+  // const responseBodyString = JSON.stringify({
+  //   query: `
+  //   mutation insertUser($id: String, $email:String, $name:String){
+  //     insert_bikepartstracker_user(objects: {id: $id, email: $email, name: $name}) {
+  //       affected_rows
+  //     }
+  //   }    
+  // `,
+  //   variables: {
+  //     id: user.id,
+  //     email: user.email,
+  //     name: user.user_metadata.full_name
+  //   }
+  // });
 
-  const result = await fetch(
-    "https://neutral-stud-43.hasura.app/v1/graphql",
-    {
-      method: "POST",
-      body: responseBodyString,
-      headers: {
-        "Content-Type": "application/json",
-        "x-hasura-admin-secret":
-          process.env.HASURA_SECRET
-      }
-    }
-  );
-  const { errors, data } = await result.json();
+  // console.log(responseBodyString);
 
-  if (errors) {
-    console.log(errors);
-    return {
-      statusCode: 500,
-      body: "Something is wrong"
-    };
-  } else {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(responseBody)
-    };
-  }
+  // const result = await fetch(
+  //   "https://neutral-stud-43.hasura.app/v1/graphql",
+  //   {
+  //     method: "POST",
+  //     body: responseBodyString,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "x-hasura-admin-secret":
+  //         process.env.HASURA_SECRET
+  //     }
+  //   }
+  // );
+  // const { errors, data } = await result.json();
+
+  // if (errors) {
+  //   console.log(errors);
+  //   return {
+  //     statusCode: 500,
+  //     body: "Something is wrong"
+  //   };
+  // } else {
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(responseBody)
+  //   };
+  // }
+
+  return { statusCode: 200 }
 }
 
 export default { handler }
