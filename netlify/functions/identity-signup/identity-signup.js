c57bf908f2af068d99ddab98fc87940012e45f25
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 04:53:40 +0200

    hmpf

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -8,52 +8,77 @@ const fetch = require("node-fetch");
 
 const handler = async function (event) {
   const data = JSON.parse(event.body)
-   const { user } = data
+  const { user } = data
 
-   console.log('USER: ', user)
+  console.log('USER: ', user)
 
   const responseBody = {
-    query: `
-    mutation insertUser($id: String, $email:String, $name:String){
-      insert_bikepartstracker_user(objects: {id: $id, email: $email, name: $name}) {
-        affected_rows
-      }
-    }    
-  `,
-    variables: {
-      id: user.id,
-      email: user.email,
-      name: user.user_metadata.full_name
-    }
+    app_metadata: {
+      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
+      my_user_info: 'this is some user info',
+    },
+    user_metadata: {
+      // append current user metadata
+      ...user.user_metadata,
+      custom_data_from_function: 'hurray this is some extra metadata',
+    },
   }
-
-  const result = await fetch(
-    "https://neutral-stud-43.hasura.app/v1/graphql",
-    {
-      method: "POST",
-      body: JSON.stringify(responseBody),
-      headers: {
-        "Content-Type": "application/json",
-        "x-hasura-admin-secret":
-          process.env.HASURA_SECRET
-      }
-    }
-  );
-
-  const { errors } = await result.json();
-
-  if (errors) {
-    console.log(errors);
-    return {
-      statusCode: 500,
-      body: "Something is wrong"
-    };
-  } else {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(responseBody)
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
   }
 }
 
 module.exports = { handler }
+
+// const handler = async function (event) {
+//   const data = JSON.parse(event.body)
+//   const { user } = data
+
+//   console.log('USER: ', user)
+
+//   const responseBody = {
+//     query: `
+//       mutation insertUser($id: String, $email:String, $name:String){
+//         insert_bikepartstracker_user(objects: {id: $id, email: $email, name: $name}) {
+//           affected_rows
+//         }
+//       }    
+//     `,
+//     variables: {
+//       id: user.id,
+//       email: user.email,
+//       name: user.user_metadata.full_name
+//     }
+//   }
+
+//   const result = await fetch(
+//     "https://neutral-stud-43.hasura.app/v1/graphql",
+//     {
+//       method: "POST",
+//       body: JSON.stringify(responseBody),
+//       headers: {
+//         "Content-Type": "application/json",
+//         "x-hasura-admin-secret":
+//           process.env.HASURA_SECRET
+//       }
+//     }
+//   );
+
+//   const { errors } = await result.json();
+
+//   if (errors) {
+//     console.log(errors);
+//     return {
+//       statusCode: 500,
+//       body: "Something is wrong"
+//     };
+//   } else {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(responseBody)
+//     };
+//   }
+// }
+
+// module.exports = { handler }
