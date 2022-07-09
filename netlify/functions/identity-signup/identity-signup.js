0ed319c6ecae1b428b087cc98ff73286e95cae17
Sebastian Engel <sebastianengel@posteo.de>
2022-07-09 04:58:42 +0200

    bubatz

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -4,6 +4,7 @@
 // more:
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
+const fetch = require('node-fetch')
 
 const handler = async function (event) {
   const data = JSON.parse(event.body)
