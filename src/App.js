ed91010a91b160f389121536088b70c8bc750aef
Sebastian Engel <sebastianengel@posteo.de>
2022-07-10 18:22:59 +0200

    Working login/logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // Import everything needed to use the `useQuery` hook
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from '@apollo/client';
-import { useIdentityContext, IdentityContextProvider } from "react-netlify-identity-widget";
-import 'react-netlify-identity-widget/styles.css'
-import "@reach/tabs/styles.css"
+import GoTrue from 'gotrue-js';
+// import { useIdentityContext, IdentityContextProvider } from "react-netlify-identity-widget";
+// import 'react-netlify-identity-widget/styles.css'
+// import "@reach/tabs/styles.css"
 
-const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
+// const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
 
 const BIKES = gql`
   query GetBikes {
@@ -39,11 +40,14 @@ const BIKES = gql`
   }
 `
 
+const auth = new GoTrue({
+  APIUrl: 'https://deploy-preview-8--taupe-alpaca-7cf6ad.netlify.app/.netlify/identity',
+  audience: '',
+  setCookie: false,
+});
+
 function DisplayUser() {
-  const identity = useIdentityContext()
   const { loading, error, data } = useQuery(BIKES);
-  
-  if (!identity?.isLoggedIn) return
 
   if (loading) return <p>Loading...</p>;
   if (error) {
@@ -52,7 +56,7 @@ function DisplayUser() {
     return <p>Error :(</p>
   }
 
-  return data.bikepartstracker_bike.map(({id, name, discipline: {name: discipline_name}}) => (
+  return data.bikepartstracker_bike.map(({ id, name, discipline: { name: discipline_name } }) => (
     <div key={id}>
       <h3>{name} ({discipline_name})</h3>
     </div>
@@ -60,56 +64,111 @@ function DisplayUser() {
 }
 
 export default function App() {
-  const url = 'https://deploy-preview-8--taupe-alpaca-7cf6ad.netlify.app/';
+  // const url = 'https://deploy-preview-8--taupe-alpaca-7cf6ad.netlify.app/';
 
   return (
-    <IdentityContextProvider url={url}>
+    // <IdentityContextProvider url={url}>
+    <>
       <AuthStatusView />
       <div>
         <h2>My first Apollo app 🚀</h2>
         <DisplayUser />
         <br />
       </div>
-    </IdentityContextProvider>
+    </>
+    // </IdentityContextProvider>
   );
 }
 
 function AuthStatusView() {
-  const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
+  // const identity = useIdentityContext()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
   // console.log('IDENTITY: ', identity)
   // console.log('TOKEN: ', identity?.user?.token?.access_token)
-  const name = (identity?.user?.user_metadata && identity?.user?.user_metadata.full_name) || 'NoName'
-  const avatar_url = identity?.user?.user_metadata?.avatar_url
+  // setUser(auth.currentUser())
+  console.log('USER: ', user)
+  let name = ''
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {identity?.isLoggedIn ? (
-          <>
-            <h1> hello {name}!</h1>
-            {avatar_url && <img alt="user name" src={avatar_url} style={{ height: 100, borderRadius: '50%' }} />}
-            <button className="btn" style={{ maxWidth: 400, background: 'orangered' }} onClick={() => setDialog(true)}>
-              LOG OUT
-            </button>
-          </>
-        ) : (
-          <>
-            <h1> hello! try logging in! </h1>
-            <button className="btn" style={{ maxWidth: 400, background: 'darkgreen' }} onClick={() => setDialog(true)}>
+  if (user) {
+    name = user.user_metadata.full_name
+  }
+
+  const login = (event) => {
+    event.preventDefault()
+    console.log('AUTH:', auth)
+    auth
+      .login(email, password)
+      .then(response => {
+        console.log('LOGIN Response: ', response)
+        localStorage.setItem('gotrue.user', JSON.stringify(response));
+        setUser(response)
+      })
+      .catch(error => console.log('LOGIN ERROR: ', error));
+  }
+
+  const logout = () => {
+    user
+      .logout()
+      .then(response => {
+        console.log("User logged out")
+        localStorage.setItem('gotrue.user', '');
+        setUser(null)
+      })
+      .catch (error => {
+        console.log("Failed to logout user: %o", error);
+        throw error;
+      });
+}
+
+return (
+  <div className="App">
+    <header className="App-header">
+      {user ? (
+        <>
+          <h1> hello {name}!</h1>
+          <button className="btn" style={{ maxWidth: 400, background: 'orangered' }} onClick={logout}>
+            LOG OUT
+          </button>
+        </>
+      ) : (
+        <>
+          <h1> hello! try logging in! </h1>
+          {/* <button className="btn" style={{ maxWidth: 400, background: 'darkgreen' }} onClick={() => setDialog(true)}>
               LOG IN
-            </button>
-          </>
-        )}
+            </button> */}
 
-        <IdentityModal
+          <form name="login" onSubmit={login}>
+            <p>
+              <label>Email
+                <input type="email" name="email" value={email} required="" onChange={(event) => setEmail(event.target.value)} />
+              </label>
+            </p>
+            <p>
+              <label>Password
+                <input type="password" name="password" value={password} required="" onChange={(event) => setPassword(event.target.value)} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Log in</button>
+            </p>
+          </form>
+
+
+
+        </>
+      )}
+
+      {/* <IdentityModal
           showDialog={dialog}
           onCloseDialog={() => setDialog(false)}
           onLogin={(user) => console.log('hello ', user?.user_metadata)}
           onSignup={(user) => console.log('welcome ', user?.user_metadata)}
           onLogout={() => console.log('bye ', name)}
-        />
-      </header>
-    </div>
-  )
+        /> */}
+    </header>
+  </div>
+)
 }
