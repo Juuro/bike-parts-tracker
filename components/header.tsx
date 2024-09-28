71d152a4426e04bdd07ea5163570d7aa62a4d8ea
Sebastian Engel <sebastianengel@posteo.de>
2024-09-28 21:59:02 +0200

    fix: wip

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,15 @@
 import React from "react";
+import { MainNav } from "./main-nav";
+import UserButton from "./user-button";
 
 const Header = () => {
   return (
-    <footer className="bg-white mt-8 inset-x-0 bottom-0 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 justify-between flex items-center py-3">
-        <div className="order-2">
-          <a href="#" className="">
-            <span className="text-xs text-slate-400">Mastodon</span>
-          </a>
-        </div>
-        <div className="order-1">
-          <p className="text-xs text-slate-400">
-            © 2024 Bike Parts Tracker All rights reserved.
-          </p>
-        </div>
+    <nav className="sticky fixed flex justify-center border-b bg-slate-50 shadow">
+      <div className="flex items-center justify-between mx-auto w-full h-16 max-w-7xl px-4 sm:px-6 lg:px-8">
+        <MainNav />
+        <UserButton />
       </div>
-    </footer>
+    </nav>
   );
 };
 
