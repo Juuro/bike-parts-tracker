d5fe730364f6867c1870d8f69fb75520f5d192ca
Sebastian Engel <sebastianengel@posteo.de>
2025-08-06 18:01:41 +0200

    chore: Tailwind 3.4.17 works

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,30 +58,19 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin 3s linear infinite",
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require("@tailwindcss/forms"),
-    function ({ addComponents }) {
-      addComponents({
-        a: {
-          "@apply text-blue-500 hover:text-blue-700 hover:underline": {},
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
