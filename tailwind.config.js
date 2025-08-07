599ce5b028dc402f5a2e48d91399255547b1716b
Sebastian Engel <sebastianengel@posteo.de>
2025-08-07 18:42:16 +0200

    chore: use tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "selector",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+  darkMode: "selector",
   theme: {
     container: {
       center: true,
